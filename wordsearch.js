293bfad63f1028b8e153445c1a3f123b16a276bf
Irha Ali <irhaali_97@hotmail.ca>
2022-12-14 21:42:14 -0500

    Implemented and tested wordSearch
    Co-authored-by: Sheila101 <Sheila101@users.noreply.github.com>

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if ((letters.length === 0) || (word.length === 0)) {
+        return false;
     }
-}
+    // Create the horrizontal join
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = new Array(horizontalJoin[0].length);
+    // Check horizontally
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+        // Created the vertical join
+        for (let i = 0; i < l.length; i++) {
+            (verticalJoin[i] === undefined) ? verticalJoin[i] = l[i] : verticalJoin[i] += l[i];
+        }
+    }
+    // Check vertically
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
